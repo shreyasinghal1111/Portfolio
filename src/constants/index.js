dbd95f0712afee5c2632f7e4d1eb42ba51c4680e
shreyasinghal1111 <singhalshreya587@gmail.com>
2024-07-01 00:19:04 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,242 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    chat,
+    images,
+    t,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "3Dimensional Visuals",
+      icon: mobile,
+    },
+    {
+      title: "TeamWork",
+      icon: backend,
+    },
+    {
+      title: "Communication",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Apple Vision Pro",
+      company_name: "GSAP and React",
+      icon: images,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        " Demonstrates mastery of React's component-driven approach for robust content management and scalability.",
+" Showcases GSAP's TimelineMax capabilities to create sophisticated animations, smooth transitions, and engaging interactive elements.",
+" Pioneers responsive design strategies, guaranteeing seamless functionality across various devices and screen sizes.",
+" Implements cutting-edge performance optimizations like code splitting and tailored animation techniques for fluid user experiences.",
+" Integrates backend APIs seamlessly to deliver real-time updates, ensuring dynamic content and maintaining Apple's renowned standards for aesthetic and functional excellence.",
+           ],
+    },
+    
+    {
+      title: "ChatNest",
+      company_name: "React,Socket.io,Tailwind css",
+      icon: chat,
+      iconBg: "#383E56",
+      points: [
+        " Facilitates real-time communication with instant messaging capabilities.",
+" Features a user-friendly interface designed for intuitive navigation.",
+" Built to handle high scalability and performance under heavy loads.",
+" Implements strong security measures including encryption and authentication.",
+" Developed using WebSocket, React.js, Node.js, and Express.js for full-stack proficiency.",
+      ],
+    },
+    {
+      title: "Covid19 Tracker",
+      company_name: "React,Express.js,Node.js",
+      icon: t,
+      iconBg: "#E6DEDD",
+      points: [
+        " Provides real-time COVID-19 statistics globally and locally.",
+" Utilizes React for interactive data visualization through charts and graphs.",
+" Features a user-friendly interface designed for easy navigation.",
+" Integrates with reliable APIs for fetching up-to-date pandemic data.",
+" Serves as an educational resource by presenting insights into COVID-19 trends and statistics.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
